e15f1dab36ccc0e3b2738feac64ca8825d247a85
Chris M <cmeredith@g.ucla.edu>
2026-03-05 09:52:50 -0800

    Add files via upload

--- photonCalcInterp.m
+++ photonCalcInterp.m
@@ -1,4 +1,4 @@
-function [out] = photonCalcInterp(calFileLoc, wavelength, voltages, ND, labelStr)
+function [out] = photonCalcInterp(calFileLoc, wavelength, voltages, ND, device)
     % Load the calibration data
     load(calFileLoc);
     
@@ -7,17 +7,22 @@ function [out] = photonCalcInterp(calFileLoc, wavelength, voltages, ND, labelStr
     thisStruct = result(idx);
     if nargin >= 5
         try
-            idx = [thisStruct.monitorDevice] == labelStr;
+            idx = strcmp({thisStruct.monitorDevice}, device);
             thisStruct = thisStruct(idx);
         catch
-            error('No matching label found in calibration file. Check for mismatch between calibration monitorDevice and function input labelStr.');
+            error('No matching label found in calibration file. Check for mismatch between calibration ''monitorDevice'' and function input ''device''.');
         end
     elseif nargin < 5 && length(thisStruct) > 1
-        warning('Warning: More than one LED found. Specify desired LED with labelStr. Defaulting to first LED in calibration file.')
+        warning('More than one LED found. Specify desired LED with device. Defaulting to first LED in calibration file.')
         thisStruct = thisStruct(1);
     else
     end
 
+    % Quick check to see if thisStruct is empty
+    if isempty(thisStruct)
+        error('Calibration struct is empty. Most likely a mismatch between ''device'' label input and ''monitorDevice'' label in calibration file');
+    else
+    end
     % Extract calibration parameters
     p = thisStruct.photonDensity;
     v = thisStruct.stimVoltage;
